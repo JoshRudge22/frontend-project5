58abd8eea97d95e2f47314f5ba1c85cfc169d5b7
JoshRudge22 <139856712+JoshRudge22@users.noreply.github.com>
2024-10-09 20:03:40 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,57 +1,57 @@
-import NavBar from './components/NavBar'
+import React, { lazy, Suspense } from 'react';
+import NavBar from './components/NavBar';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
-import SignInForm from './pages/auth/SignInForm';
-import SignUpForm from './pages/auth/SignUpForm';
-import ProfilePage from './pages/profile/info/ProfilePage';
-import EditProfile from './pages/profile/update/EditProfile';
-import DeleteProfile from './pages/profile/other/DeleteProfile';
-import UsersPage from './pages/profile/other/UsersPage';
-import CreatePost from './pages/posts/CreatePost';
-import PostList from './pages/posts/PostList'
-import OtherUsersPostList from './pages/profile/other/OtherUsersPostList';
-import FeedPage from './pages/feed/FeedPage';
-import CommentsList from './pages/interactions/CommentsList'
-import LikeList from './pages/interactions/LikesList';
-import LikedPost from './pages/interactions/LikedPost';
-import FollowingFeedPage from './pages/feed/FollowingFeedPage'
-import MyFollowersList from './pages/follow/MyFollowersList'
-import MyFollowingList from './pages/follow/MyFollowingList'
-import ContactForm from './pages/contact/ContactForm'
-import FormSubmitted from './pages/contact/FormSubmmited';
 import NotFound from './components/NotFound';
 
-
-
-
+const SignInForm = lazy(() => import('./pages/auth/SignInForm'));
+const SignUpForm = lazy(() => import('./pages/auth/SignUpForm'));
+const ProfilePage = lazy(() => import('./pages/profile/info/ProfilePage'));
+const EditProfile = lazy(() => import('./pages/profile/update/EditProfile'));
+const DeleteProfile = lazy(() => import('./pages/profile/other/DeleteProfile'));
+const UsersPage = lazy(() => import('./pages/profile/other/UsersPage'));
+const CreatePost = lazy(() => import('./pages/posts/CreatePost'));
+const PostList = lazy(() => import('./pages/posts/PostList'));
+const OtherUsersPostList = lazy(() => import('./pages/profile/other/OtherUsersPostList'));
+const FeedPage = lazy(() => import('./pages/feed/FeedPage'));
+const CommentsList = lazy(() => import('./pages/interactions/CommentsList'));
+const LikeList = lazy(() => import('./pages/interactions/LikesList'));
+const LikedPost = lazy(() => import('./pages/interactions/LikedPost'));
+const FollowingFeedPage = lazy(() => import('./pages/feed/FollowingFeedPage'));
+const MyFollowersList = lazy(() => import('./pages/follow/MyFollowersList'));
+const MyFollowingList = lazy(() => import('./pages/follow/MyFollowingList'));
+const ContactForm = lazy(() => import('./pages/contact/ContactForm'));
+const FormSubmitted = lazy(() => import('./pages/contact/FormSubmitted'));
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Container>
-        <Switch>
-          <Route exact path="/" render={() => <FeedPage />} />
-          <Route exact path="/feed" render={() => <FollowingFeedPage />} />
-          <Route exact path="/signin" render={() => <SignInForm />} />
-          <Route exact path="/signup" render={() => <SignUpForm />} />
-          <Route exact path="/profiles/:profileId" render={() => <ProfilePage />} />
-          <Route exact path="/edit/:profileId" render={() => <EditProfile />} />
-          <Route exact path="/profile/:username" render={(props) => <UsersPage {...props} />} />
-          <Route exact path="/user/:username/posts" render={(props) => <OtherUsersPostList {...props} />} />
-          <Route exact path="/profiles/delete/:profileId" render={() => <DeleteProfile />} />
-          <Route exact path="/posts/create" render={() => <CreatePost />} />
-          <Route exact path="/posts/list" render={() => <PostList />} />
-          <Route exact path="/commentslist" render={() => <CommentsList />} />
-          <Route exact path="/posts/:postId/likes" render={({ match }) => <LikeList postId={match.params.postId} />} />
-          <Route exact path="/users/liked-posts" render={({ match }) => <LikedPost postId={match.params.postId} />} />
-          <Route exact path="/followerslist" render={() => <MyFollowersList />} />
-          <Route exact path="/followinglist" render={() => <MyFollowingList />} />
-          <Route exact path="/contact" render={() => <ContactForm />} />
-          <Route exact path="/submitted" render={() => <FormSubmitted />} />
-          <Route render={() => <NotFound />} />
-        </Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route exact path="/" component={FeedPage} />
+            <Route exact path="/feed" component={FollowingFeedPage} />
+            <Route exact path="/signin" component={SignInForm} />
+            <Route exact path="/signup" component={SignUpForm} />
+            <Route exact path="/profiles/:profileId" component={ProfilePage} />
+            <Route exact path="/edit/:profileId" component={EditProfile} />
+            <Route exact path="/profile/:username" component={UsersPage} />
+            <Route exact path="/user/:username/posts" component={OtherUsersPostList} />
+            <Route exact path="/profiles/delete/:profileId" component={DeleteProfile} />
+            <Route exact path="/posts/create" component={CreatePost} />
+            <Route exact path="/posts/list" component={PostList} />
+            <Route exact path="/commentslist" component={CommentsList} />
+            <Route exact path="/posts/:postId/likes" component={LikeList} />
+            <Route exact path="/users/liked-posts" component={LikedPost} />
+            <Route exact path="/followerslist" component={MyFollowersList} />
+            <Route exact path="/followinglist" component={MyFollowingList} />
+            <Route exact path="/contact" component={ContactForm} />
+            <Route exact path="/submitted" component={FormSubmitted} />
+            <Route component={NotFound} />
+          </Switch>
+        </Suspense>
       </Container>
     </div>
   );
