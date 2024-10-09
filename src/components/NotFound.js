9686ffab6c3b0a63361bfc6eccfb64783e3d2e5e
JoshRudge22 <139856712+JoshRudge22@users.noreply.github.com>
2024-10-09 19:28:36 +0500

    Update NotFound.js

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,14 +1,24 @@
-import React from 'react'
-import NoResults from '../media/no-results.png'
-import notFoundStyles from '../styles/Noresults.module.css'
+import React from 'react';
+import NoResults from '../media/no-results.png';
+import notFoundStyles from '../styles/Noresults.module.css';
 import Asset from "./Assets";
+import { NavLink } from 'react-router-dom';
 
 const NotFound = () => {
-    return (
-      <div className={notFoundStyles.notFound}>
-        <Asset src={NoResults} message="Sorry, the page you're looking for doesn't exist" />
+  return (
+    <div className={notFoundStyles.notFound}>
+      <Asset 
+        src={NoResults} 
+        message="Sorry, the page you're looking for doesn't exist" 
+        altMessage="No results found" // Added alt message
+      />
+      <div className={notFoundStyles.navigation}>
+        <NavLink to="/" className={notFoundStyles.link}>
+          Go back to Home
+        </NavLink>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-export default NotFound
+export default NotFound;
