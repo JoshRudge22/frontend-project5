15b0286a9e895ad6afac14d927c6c09cd04c2e8d
JoshRudge22 <139856712+JoshRudge22@users.noreply.github.com>
2024-10-09 20:03:54 +0500

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,8 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  //render(<App />);
-  //const linkElement = screen.getByText(/learn react/i);
-  //expect(linkElement).toBeInTheDocument();
+  render(<App />); // Render the App component
+
+  const linkElement = screen.getByText(/learn react/i); // Search for the text
+  expect(linkElement).toBeInTheDocument(); // Assert that the text is present in the document
 });
