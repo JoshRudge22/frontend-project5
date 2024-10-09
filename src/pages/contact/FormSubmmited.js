9df18966e0d8aafb4aa86285bd0d81af2935c1f8
JoshRudge22 <139856712+JoshRudge22@users.noreply.github.com>
2024-10-09 19:45:35 +0500

    Update FormSubmmited.js

--- src/pages/contact/FormSubmmited.js
+++ src/pages/contact/FormSubmmited.js
@@ -1,22 +1,27 @@
 import React from 'react';
-import submittedStyles from '../../styles/contact/Submitted.module.css'
+import submittedStyles from '../../styles/contact/Submitted.module.css';
 
 const FormSubmitted = () => {
   return (
-    <div className={submittedStyles.container}>
+    <div className={submittedStyles.container} role="alert">
       <h2 className={submittedStyles.title}>Form Submitted Successfully!</h2>
       <p className={submittedStyles.message}>
-        Thank you for submitting the form, we always aim to be in touch within 2 working days from when the form was submitted.
+        Thank you for submitting the form. We always aim to be in touch within 2 working days from when the form was submitted.
       </p>
       <p className={submittedStyles.message}>
-        If you haven't heard back in 2 working days you can either email, call or write to us with the information below.
+        If you haven't heard back in 2 working days, you can either email, call, or write to us with the information below.
       </p>
       <ul className={submittedStyles.contactList}>
-        <li>Email: <a className={submittedStyles.link} href="mailto:joshrudge@hotmail.com">joshrudge@hotmail.com</a></li>
-        <li>Phone Number: 01925 000000</li>
+        <li>
+          Email: <a className={submittedStyles.link} href="mailto:joshrudge@hotmail.com">joshrudge@hotmail.com</a>
+        </li>
+        <li>
+          Phone Number: <a className={submittedStyles.link} href="tel:01925000000">01925 000000</a>
+        </li>
         <li>Address: 133 Something, New City, WA2 44ET</li>
       </ul>
-      <p className={submittedStyles.message}>Please check junk mail</p>
+      <p className={submittedStyles.message}>Please check your junk mail folder if you don't see our response.</p>
+      <button className={submittedStyles.button} onClick={() => window.location.href = '/'}>Back to Home</button>
     </div>
   );
 };
